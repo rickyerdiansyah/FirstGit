f242996fea2fb0f18faa9e46e7da213e86eb6ce5
Ricky Erdiansyah <erdiansyah.ricky@gmail.com>
2019-05-15 08:25:55 +0700

    Add save coordinate function

--- FirstGit/ViewController.swift
+++ FirstGit/ViewController.swift
@@ -45,6 +45,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+        
+        print("Save coordinate")
     }
 
     // MARK: - ARSCNViewDelegate
